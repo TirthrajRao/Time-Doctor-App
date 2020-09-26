21c0b298b617326abc7b07d87ec434f344aa8193
mohitkachhadiya <mohitkachhadiya312@gmail.com>
2020-09-26 21:07:50 +0530

    ScreenShot Capture Successfully

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,19 +18,17 @@ export class HomeComponent implements OnInit {
   userInfo = JSON.parse(localStorage.getItem('currentUser'));
   base64data:any;
   baseArray:any = [];
-  post:any = {
-    userId: this.userInfo,
-    image: JSON.parse(localStorage.getItem('imgUrl'))
-  }
 
-  constructor(public _userService: UserService, public router: Router, private formBuilder: FormBuilder) { 
+  constructor(public _userService: UserService, public router: Router) { 
   }
 
   ngOnInit() {
-  	if (this.userInfo) {
+    if (this.userInfo) {
       setInterval(() => {
+        localStorage.removeItem('imgUrl');
         this.callexternalfunction();
       }, 60000);
+
     }
     else{
       this.router.navigate(['/login']);
@@ -38,39 +36,38 @@ export class HomeComponent implements OnInit {
     }
   }
 
-    callexternalfunction(){
-
+  callexternalfunction(){
     externalFunction();
-    this.base64data = JSON.parse(localStorage.getItem("imgUrl"));
-    this.baseArray.push(this.base64data);
 
-    this._userService.uploadbase64Img(this.base64data).subscribe((res) => {
-      console.log("the res is the ==========>", res);
-      // this._userService.uploadImage(this.post).subscribe((res) => {
-        //   console.log("the res add post is ====>", res);
-        // }, (err) => {
-          //   console.log("the err add post is ====>", err);
-          // })
-        }, (err) => {
-          console.log("the err is the ==========>", err);
-        })
-    this.baseArray.forEach((item, index) => {
-    	console.log("the item is the =====>", item);
-    	if (item == null) {
-    		console.log("array null");
-    	}
-    	else{
-	      document.getElementById('mypreview').setAttribute("src", this.base64data);
-    	}
-    });
-    console.log("the baseArray is the ====>", this.baseArray);
+    console.log("local called");
+    setTimeout(() => {
+      this.base64data = JSON.parse(localStorage.getItem("imgUrl"));
+      console.log("the item get is the ===>", JSON.parse(localStorage.getItem("imgUrl")));
+      this.baseArray.push(this.base64data);
+      console.log("the item get is the ===>", this.base64data);
+
+      this._userService.uploadbase64Img(this.base64data).subscribe((res) => {
+        console.log("the res is the ==========>", res);
+      }, (err) => {
+        console.log("the err is the ==========>", err);
+      })
+      this.baseArray.forEach((item, index) => {
+        if (item == null) {
+          console.log("array null");
+        }
+        else{
+          document.getElementById(index).setAttribute("src", this.base64data);
+        }
+      });
+      console.log("the baseArray is the ====>", this.baseArray);
+    }, 500)
   }
 
   logout(){
     localStorage.removeItem('currentUser');
     localStorage.removeItem('imgUrl');
-        // clearInterval(this.intervalId);
-        this.router.navigate(['login']);
+    // clearInterval(this.intervalId);
+    this.router.navigate(['login']);
   }
 
 }
