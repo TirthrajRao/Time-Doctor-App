d807b532c6112e1fc31cc5adec7c336e9756c4a7
mehulbhatt45 <mehul.2287884@gmail.com>
2020-09-28 19:29:42 +0530

    Auto-generated commit

--- assets/externalJs/externalFunction.js
+++ assets/externalJs/externalFunction.js
@@ -1,6 +1,6 @@
 "use strict";
+const {desktopCapturer} = require('electron');
 function externalFunction() {
-  const {desktopCapturer} = require('electron');
   
       fullscreenScreenshot(function(base64data){
                   localStorage.setItem('imgUrl', JSON.stringify(base64data));
